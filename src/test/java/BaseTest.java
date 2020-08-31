6d9d2d76edde3da33423e956faa769ef936c10db
Diep Anh <nguyendiepanh@gobiz.vn>
2020-08-31 17:32:41 +0700

    Sửa code

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,26 +1,27 @@
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
 
 import java.util.concurrent.TimeUnit;
 
-public class BaseTest {
 
-    WebDriver driver;
+public class BaseTest {
+    public static WebDriver driver;
     public static final String URL_login = "https://athena-staging.cinnamon.is/login";
     public static final String URL_dashBoard = "https://athena-staging.cinnamon.is/data?offset=0&limit=10&sort_by=date_modified&order=DESC";
-    @BeforeMethod
-    public void setUp() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
+
+    @BeforeClass
+    public static void setup() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
         driver = new ChromeDriver();
         driver.get(URL_login);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
     }
 
-    @AfterMethod
-    public void tearDown() {
+    @AfterClass
+    public static void tearDown() {
         driver.quit();
     }
-
 }
---
+++ src/test/java/TestDemoStep.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+import org.openqa.selenium.By;
+
+public class TestDemoStep extends BaseTest {
+    String username = "username";
+    String password = "password";
+    String submitBtn = "//button[text() ='Log in']";
+
+    public void login(String userName, String passWord) {
+        driver.findElement(By.id(username)).sendKeys(userName);
+        driver.findElement(By.id(password)).sendKeys(passWord);
+        driver.findElement(By.xpath(submitBtn)).click();
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @Test
+    public void loginWithBlankField() {
+        login("", "");
+        Assert.assertEquals(driver.getCurrentUrl(), BaseTest.URL_login);
+    }
+    @Test
+    public void loginWithInvalid() {
+        login("azure", "123");
+        String loginErrActual = driver.findElement(By.xpath("//div[contains(text(),'Invalid username or password.')]")).getText();
+        String loginErrExpect = "Invalid username or password.";
+        Assert.assertEquals(loginErrActual, loginErrExpect);
+    }
+    @Test
+    public void loginByMember() {
+        login("lara", "ToQuyen.95");
+        Assert.assertEquals(driver.getCurrentUrl(), BaseTest.URL_dashBoard);
+    }
+}
