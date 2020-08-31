2c900e570c68e26efae72cbc446a26cd2bf8cb97
Lara <lara@cinnamon.is>
2020-08-31 18:55:37 +0700

    daft

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,5 +1,5 @@
-import org.junit.AfterClass;
-import org.junit.BeforeClass;
+import org.junit.After;
+import org.junit.Before;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
@@ -9,19 +9,19 @@ import java.util.concurrent.TimeUnit;
 public class BaseTest {
     public static WebDriver driver;
     public static final String URL_login = "https://athena-staging.cinnamon.is/login";
-    public static final String URL_dashBoard = "https://athena-staging.cinnamon.is/data?offset=0&limit=10&sort_by=date_modified&order=DESC";
+    public static final String URL_dashBoard = "https://athena-staging.cinnamon.is/data?offset=0&limit=100&sort_by=date_modified&order=DESC";
 
-    @BeforeClass
-    public static void setup() {
-        System.setProperty("webdriver.chrome.driver", "driver/chromedriver");
+    @Before
+    public void setup() {
+        System.setProperty("webdriver.chrome.driver", "driver/chromedriver.exe");
         driver = new ChromeDriver();
         driver.get(URL_login);
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
 
     }
 
-    @AfterClass
-    public static void tearDown() {
+    @After
+    public void tearDown() {
         driver.quit();
     }
 }
